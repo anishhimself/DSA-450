92738786abe0cec1a9990aa4eb9106a0b580ea90
anishhimself <57608042+anishhimself@users.noreply.github.com>
2023-12-10 13:31:13 +0545

    reverse array problem

--- Array/ReverseTheArray.py
+++ Array/ReverseTheArray.py
@@ -1,2 +1,12 @@
-def reverseArray():
-    pass
+def reverseArray(arr, start, end):
+    
+    while start<end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start+=1
+        end-=1
+        
+arr = [1,2,3,4,5,6,7]
+print(arr)
+reverseArray(arr, 0, 6)
+print("The reverse list is")
+print(arr)
